a73292d800d53ee7658f493e0c503eed8a90eb5a
Wortelnich <firza7004@gmail.com>
2023-02-03 15:58:10 +0700

    feat: tailwind & login

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import LoginView from '../views/LoginView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      name: 'Login',  
+      component: LoginView
     },
     {
       path: '/about',
@@ -20,4 +20,4 @@ const router = createRouter({
   ]
 })
 
-export default router
+export default router;
