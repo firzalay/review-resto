5552049c68f1a808eaf06b9144c3cb966c70b699
Wortelnich <firza7004@gmail.com>
2023-02-08 20:59:51 +0700

    feat(restoDetail): display reviews

--- src/views/RestoDetailView.vue
+++ src/views/RestoDetailView.vue
@@ -11,30 +11,63 @@ const repository = useRestoRepository();
 
 const route = useRoute();
 const isLoading = ref(true);
-const detail = ref({});
+const resto = ref({});
 const fetchDetail = async () => {
   isLoading.value = true;
   try {
     const id = route.params.id;
     const { data } = await repository.show(id);
-    detail.value = data;
+    resto.value = data;
   } catch (e) {
     console.log(e);
   }
   isLoading.value = false;
 };
 onMounted(() => fetchDetail());
+
+const reviews = ref([]);
+const fetchReviews = async () => {
+  isLoading.value = true;
+  try {
+    const id = route.params.id;
+    const { data } = await repository.reviews(id);
+    reviews.value = data;
+  } catch (e) {
+    console.log(e);
+  }
+  isLoading.value = false;
+};
+onMounted(() => fetchReviews());
+
 </script>
 
 <template>
+     <BaseCard class="mt-4 bg-red-600 text-white">
+      <template #title>Resto</template>
+    </BaseCard>
+
+
+
+
     <BaseContainer>
         <RouterLink
       :to="{ name: 'restos' }"
       class="inline-block p-2 px-4 bg-blue-600 text-white rounded hover:bg-blue-800">Back</RouterLink>
 
-      <BaseCard class="mt-4">
-      <template #title>{{ detail.name }}</template>
+    <BaseCard class="mt-4">
+        <template #title>{{ resto.name }}</template>
+        {{ resto.description }}    
     </BaseCard>
-    <BaseCard class="mt-4">{{ detail.description }}</BaseCard>
+
+    <BaseCard class="mt-4 bg-red-600 text-white">
+      <template #title>Reviews</template>
+    </BaseCard>
+
+
+    <BaseCard v-for="review in reviews" :key="review.id" class="mt-4">
+      <template #title>{{ review.user.name }}</template>
+      {{ review.text }}
+    </BaseCard>
+
   </BaseContainer>
 </template>
